a053d7b98eaad8512dd1ffa3605de2b78cf0ed90
Jose <sixgg4@gmail.com>
2018-06-30 18:33:28 -0400

    first commit

---
+++ javanFun/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { //name the class the same as the file. Strict convention Needs to match
+    public static void main(String[]args){ //this is the entry point method for our program to run.
+         System.out.println("Hello World");
+    }
+}
