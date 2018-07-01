2981b57dcd79ac05e293680d63bbdbffb13db566
Jose <sixgg4@gmail.com>
2018-07-01 17:34:11 -0400

    Second assignment uploaded.

---
+++ helloDojo/HelloDojo.java
@@ -0,0 +1,29 @@
+public class HelloDojo {
+    public static void main(String[] args) {
+        // int age; //here's the name
+        // double is used for fractions
+        // int age = 28; // here's the assignment operator equaling 28
+        String greeting = "My name is Sixto. ";
+        String age = "I am 28 years old. ";
+        String location = " My Hometown is Manassas, VA. ";
+        String str = greeting + " " + age + location;
+
+        // System.out.println(greeting + age + "." + location);
+        System.out.println(str.replaceAll("  ", "\n"));
+        System.out.println();
+    }
+}
+
+// My name is Coding Dojo
+// I am 100 years old
+// My hometown is Burbank, CA
+
+// system.out.print('insert here') this will print it in a line
+// system.out.println('insert here') this will print it in anything after it in
+// a line
+
+// String.format("%s%n%s%n%s", firstName, lastName, Time);
+// if you are using format then use the format string with arguments.
+
+// %s = String
+// %n = new line
