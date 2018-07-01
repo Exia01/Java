af834028766b967babe1812be483ab3d445d6e78
Jose <sixgg4@gmail.com>
2018-07-01 15:29:25 -0400

    Enter

--- javanFun/HelloWorld.java
+++ javanFun/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld { //name the class the same as the file. Strict convention Needs to match
     public static void main(String[]args){ //this is the entry point method for our program to run.
-         System.out.println("Hello World");
+         System.out.println("Hello World"); //system class comes with ervery java program
+         // .out is a variable of that class that returns an object of the .print ln -> 
     }
 }
